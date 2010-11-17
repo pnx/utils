ab4a6750245f3c0adcf47b02a915913a3adcb389
Henrik Hautakoski <henrik@fiktivkod.org>
2010-11-17 16:52:34 +0100

    qlreplays: added logic for removing the old target if empty when switching targets

--- qlreplays.sh
+++ qlreplays.sh
@@ -3,14 +3,24 @@
 # Simple script to be used with cronjob to change location (daily) 
 # where quakelive demos should be stored
 
-BASEDIR=~/.quakelive/quakelive/home/baseq3
-REPLAYDIR=replays/`/bin/date '+%Y-%m-%d'`
+BASEDIR=~/ql
+REPLAYDIR=replays
 
-if [ "`readlink "${BASEDIR}/demos"`" != "${REPLAYDIR}" ]; then
+OLDTARGET=$(/bin/basename `readlink -s "${BASEDIR}/demos"`)
+NEWTARGET=$(/bin/date '+%Y-%m-%d')
 
-    if [ ! -d "${BASEDIR}/${REPLAYDIR}" ]; then
-        /bin/mkdir -p "${BASEDIR}/${REPLAYDIR}"
+if [ "${REPLAYDIR}/${OLDTARGET}" != "${REPLAYDIR}/${NEWTARGET}" ]; then
+
+    # create newtarget
+    if [ ! -d "${BASEDIR}/${REPLAYDIR}/${NEWTARGET}" ]; then
+        /bin/mkdir -p "${BASEDIR}/${REPLAYDIR}/${NEWTARGET}"
     fi
     
-    /bin/ln -snf "${REPLAYDIR}" "${BASEDIR}/demos"
+    # change the symlink to point to newtarget
+    /bin/ln -snf "${REPLAYDIR}/${NEWTARGET}" "${BASEDIR}/demos"
+    
+    # remove oldtarget if empty
+    if [ `/bin/ls "${BASEDIR}/${REPLAYDIR}/${OLDTARGET}/" | /bin/wc -l` -eq 0 ]; then
+        /bin/rm -fr "${BASEDIR}/${REPLAYDIR}/${OLDTARGET}/"
+    fi
 fi
