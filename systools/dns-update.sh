c5aabd3265c307e8a0de4f84325c478a9318353b
Henrik Hautakoski <henrik@fiktivkod.org>
2014-04-01 13:59:34 +0200

    dns-update.sh: Fetch IP from /bin/ip before ifconfig

--- systools/dns-update.sh
+++ systools/dns-update.sh
@@ -1,12 +1,40 @@
 #/bin/sh
 
 CURL=/usr/bin/curl
+IFCONFIG=/sbin/ifconfig
+IP_CMD=/bin/ip
 
-# interface
 function ifconfig_inet4_addr {
 
-	/sbin/ifconfig "${1}" | grep 'inet addr' | \
-		awk '{print $2}' | sed 's/addr://' 2> /dev/null
+	if [ -x "${IFCONFIG}" ]; then
+		${IFCONFIG} "${1}" | grep -e 'inet\ \(addr\)\?' | \
+				awk '{print $2}' | sed 's/addr://' 2> /dev/null
+	fi
+}
+
+function ip_inet4_addr {
+
+	if [ ! -x "${IP_CMD}" ]; then
+		return
+	fi
+
+	PATTERN='[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
+
+	${IP_CMD} -o -f inet addr show | grep "${1}" | \
+		awk '{print $4}' | grep -Eo ${PATTERN} 2> /dev/null
+}
+
+# interface
+function inet4_addr {
+
+	IP=$(ip_inet4_addr "${1}")
+
+	# fallback on ifconfig
+	if [ "x" == "x${IP}" ]; then
+		IP=$(ifconfig_inet4_addr "${1}")
+	fi
+
+	echo $IP
 }
 
 # user
@@ -15,12 +43,17 @@ function ifconfig_inet4_addr {
 # interface
 function loopia_update {
 
-	IP=$(ifconfig_inet4_addr ${4})
+	IP=$(inet4_addr ${4})
+
+	if [ "x" == "x${IP}" ]; then
+		echo "Can't find IP for inteface '${4}'"
+		return
+	fi
 
 	for i in $(echo ${3} | tr ',' '\n'); do
 
 		echo "Updating ${IP} -> ${i}"
-	
+
 		$CURL -v --user "${1}:${2}" \
 			"http://dns.loopia.se/XDynDNSServer/XDynDNS.php?hostname=${i}&myip=${IP}"
 	done
