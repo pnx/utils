215d268759a7d5d61857e618c4aca031e625e7d8
Henrik Hautakoski <henrik@fiktivkod.org>
2014-04-24 13:22:15 +0200

    Adding gitscripts/changelog.sh

---
+++ gitscripts/changelog.sh
@@ -0,0 +1,15 @@
+
+# Builds a changelog for changes between last tag and HEAD
+
+if [ -n "${1}" ]; then
+	OUTFILE=${1}
+else
+	OUTFILE=./.changelog
+fi
+
+LAST_TAG=$(git log --tags --pretty=format:'%H' -n 1)
+
+if [ -n "${LAST_TAG}" ]; then
+	/bin/echo -e "\n\n" 1> ${OUTFILE}
+	git shortlog --no-merges ${LAST_TAG}..HEAD 1>> ${OUTFILE}
+fi
